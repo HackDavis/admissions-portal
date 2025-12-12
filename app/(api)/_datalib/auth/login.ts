bda599be478244cbf9dc63faa82657eec8bb4892
michelleyeoh <michellew.yeoh@gmail.com>
2025-12-12 01:15:27 -0800

    added missing files

---
+++ app/(api)/_datalib/auth/login.ts
@@ -0,0 +1,29 @@
+import AuthError from 'next-auth';
+
+import { signIn } from '@/auth';
+
+export default async function Login(email: string, password: string) {
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirect: false,
+    });
+
+    return { ok: true, body: 'Successfully logged in', error: null };
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return {
+        ok: false,
+        body: null,
+        error: (error as any)?.cause?.err?.message || 'Authentication error.',
+      };
+    }
+
+    return {
+      ok: false,
+      body: null,
+      error: 'Authentication error.',
+    };
+  }
+}
