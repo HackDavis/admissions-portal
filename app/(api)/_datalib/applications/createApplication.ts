f71ee94a6c690807429b77b8144ee1092171d89a
michelleyeoh <74385095+michelleyeoh@users.noreply.github.com>
2026-01-06 23:26:19 -0800

    Crud applications (#12)
    Create, delete, update, and get (CRUD) functions for applications.
    
    Checks to make sure there are no duplicate values for unique value (email).
    
    ---------
    
    Co-authored-by: alex <174984579+phlangiee@users.noreply.github.com>

---
+++ app/(api)/_datalib/applications/createApplication.ts
@@ -0,0 +1,48 @@
+import { ObjectId } from 'mongodb';
+
+import { getDatabase } from '@utils/mongodb/mongoClient.mjs';
+import isBodyEmpty from '@utils/request/isBodyEmpty';
+import parseAndReplace from '@utils/request/parseAndReplace';
+import {
+  HttpError,
+  NoContentError,
+  DuplicateError,
+} from '@utils/response/Errors';
+
+export const CreateApplication = async (body: object) => {
+  try {
+    // empty
+    if (isBodyEmpty(body)) {
+      throw new NoContentError();
+    }
+
+    const parsedBody = await parseAndReplace(body); // Delete if application has no id fields?
+
+    const db = await getDatabase();
+
+    // unique values have duplicate
+    const hasDuplicate = await db.collection('applications').findOne({
+      email: parsedBody.email,
+    });
+
+    if (hasDuplicate) {
+      throw new DuplicateError('Duplicate Error: applicant already submitted.');
+    }
+
+    const creationStatus = await db
+      .collection('applications')
+      .insertOne(parsedBody);
+    const application = await db.collection('applications').findOne({
+      _id: new ObjectId(creationStatus.insertedId),
+    });
+
+    return { ok: true, body: application, error: null };
+  } catch (e) {
+    const error = e as HttpError;
+    return {
+      ok: false,
+      body: null,
+      error: error.message,
+    };
+  }
+};
